c9a3edbf84b1ad145e376078e14dd70ad20a8e4a
Forrest <Forrestmillerj@gmail.com>
2020-07-31 15:45:30 -0500

    updated cats app.js

--- cats/src/App.js
+++ cats/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import GreetingCat from "./components/greetingCat";
+import RandomCat from "./components/randomCat";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defaultHistory = createBrowserHistory();
+
+export default function App({history = defaultHistory}) {
+  return(
+    <Router histroy={history}>
+      <Route exact path="/" component={RandomCat}/>
+      <Route exact path="/cat/:greeting" component={GreetingCat}/>
+    </Router>
+  )
 }
-
-export default App;
